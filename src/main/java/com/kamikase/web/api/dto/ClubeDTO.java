26b028e2e7624b95c7d3045f0673711a0c5da762
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-26 02:34:22 -0300

    clube dto created

--- src/main/java/com/kamikase/web/api/dto/ClubeDTO.java
+++ src/main/java/com/kamikase/web/api/dto/ClubeDTO.java
@@ -1,5 +1,14 @@
 package com.kamikase.web.api.dto;
 
+import lombok.Data;
+
+@Data
 public class ClubeDTO {
   
+    private Integer id;
+    private String nome;
+    private String estado;
+    private String email;
+    private String cnpj;
+    private String dataCriacao;
 }

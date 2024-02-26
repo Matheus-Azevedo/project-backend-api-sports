25233943595ff558e5b14461e0509d6a62fb1265
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-26 02:24:52 -0300

    validation created

--- src/main/java/com/kamikase/web/api/validator/EmailValidator.java
+++ src/main/java/com/kamikase/web/api/validator/EmailValidator.java
@@ -4,8 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 
-public class EmailValidator implements
-        ConstraintValidator<EmailValidation, String> {
+public class EmailValidator implements ConstraintValidator<EmailValidation, String> {
     private String message;
     @Override
     public void initialize(EmailValidation constraintAnnotation) {
@@ -15,7 +14,7 @@ public class EmailValidator implements
 
     @Override
     public boolean isValid(String nome, ConstraintValidatorContext constraintValidatorContext) {
-        if(nome.contains("@fuji.com")) {
+        if(nome.contains("@matheus.br")) {
             return true;
         }
         constraintValidatorContext.disableDefaultConstraintViolation();
--- src/main/java/com/kamikase/web/api/validator/NameValidation.java
+++ src/main/java/com/kamikase/web/api/validator/NameValidation.java
@@ -1,5 +1,17 @@
 package com.kamikase.web.api.validator;
 
-public class NameValidation {
+import java.lang.annotation.*;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target( { ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = NameValidator.class)
+public @interface NameValidation {
+    String message() default "Nome não é válido, tem outro";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
   
 }

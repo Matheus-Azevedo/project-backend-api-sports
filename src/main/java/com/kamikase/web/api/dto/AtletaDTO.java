12520df9a7bb4467d2ff5b20b3cff27bb68f5e10
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-02-26 02:27:01 -0300

    atleta dto created

--- src/main/java/com/kamikase/web/api/dto/AtletaDTO.java
+++ src/main/java/com/kamikase/web/api/dto/AtletaDTO.java
@@ -1,5 +1,15 @@
 package com.kamikase.web.api.dto;
 
+import lombok.Data;
+
+@Data
 public class AtletaDTO {
+
+    private Long id;
+    private String nome;
+    private Integer anoNascimento;
+    private String esporte;
+    private String email;
+    private String cpf;
   
 }
